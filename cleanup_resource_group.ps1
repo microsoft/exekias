fa437a27b0d3d995eb75b2b536da99daf4755029
Vassily Lyutsarev <vassilyl@microsoft.com>
2023-09-09 14:39:14 +0100

    use empty deployment in complete mode to cleanup

--- cleanup_resource_group.ps1
+++ cleanup_resource_group.ps1
@@ -12,48 +12,21 @@ Write-Host "[$(Get-Date)] Deleting ALL resources in resource group $resourceGrou
 
 Set-AzContext -Subscription $subscription | Out-Null
 
-function Remove-ResourceUntilAbsent
+$emptyPath = Join-Path -Path ([IO.Path]::GetTempPath()) -ChildPath "empty.json"
+$deploymentName = "cleanup_$((Get-Date).ToString("yyyyMMdd_HHmmss"))"
+@'
 {
-    process{
-        # input object must be an Azure Resource Id
-        Start-Job -ArgumentList $_  -ScriptBlock {
-            param($ResourceId)
-            Remove-AzResource -ResourceId $ResourceId -Force | Out-Null
-            while (Get-AzResource -ResourceId $ResourceId -ErrorAction SilentlyContinue){
-                Write-Information "Retry: $resourceId"
-                Start-Sleep -Seconds 10
-                Remove-AzResource -ResourceId $ResourceId -Force | Out-Null
-            }
-            $ResourceId
-        }
-    }
-}
-
-# Delete Function Apps first to unblock App Service Plans
-$apps = Get-AzWebApp -ResourceGroupName $resourceGroup
-if ($apps) {
-    Write-Host "[$(Get-Date)] Deleting web apps $($apps | Select-Object -ExpandProperty Name | Join-String -Separator ', ')..."
-    $jobs = $apps | Select-Object -ExpandProperty Id | Remove-ResourceUntilAbsent
-    Receive-Job $jobs -AutoRemoveJob -Wait
-}
-
-# Delete other resources
+    "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
+    "contentVersion": "1.0.0.0",
+    "resources": []
+  }
+'@ | Set-Content -Path $emptyPath -Encoding UTF8
+New-AzResourceGroupDeployment -Name $deploymentName  -ResourceGroupName $resourceGroup -TemplateFile $emptyPath -Mode Complete -Force
+Remove-Item -Path $emptyPath
 $resources = Get-AzResource -ResourceGroupName $resourceGroup
 if ($resources) {
-    Write-Host "[$(Get-Date)] Deleting $($resources.Length) other resources..."
-    $jobs = $resources | Select-Object -ExpandProperty ResourceId | Remove-ResourceUntilAbsent
-    Receive-Job $jobs -AutoRemoveJob -Wait
-}
-
-# Wait until ARM reports the resource group is empty
-$timeout = (Get-Date) + (New-TimeSpan -Minutes 10)
-while (((Get-Date) -lt $timeout) -and (Get-AzResource -ResourceGroupName $resourceGroup)) {
-    Start-Sleep -Seconds 1
-}
-$resources_left = Get-AzResource -ResourceGroupName $resourceGroup
-if ($resources_left) {
-    $resources_left
-    Write-Error "[$(Get-Date)] Resource group is not empty"
+    $resources | Select-Object -Property Name,ResourceType | Format-Table
+    Write-Error "Resource group is not empty"
     exit 1
 }
 
